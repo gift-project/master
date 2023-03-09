99a9b30c705a14425b39da3c42596c6b8524c31e
kingryan9996 <89970039+kingryan9996@users.noreply.github.com>
2023-03-09 18:17:55 +0900

    mod:navBar

--- pages/GiftTree/[id].jsx
+++ pages/GiftTree/[id].jsx
@@ -7,17 +7,14 @@ import { TeamC } from '../src/Context';
 import styles from "./test.module.css";
 import ProductList from '../src/component/productList';
 import NavBar from '../src/component/NavBar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 
 const GiftTree = () => {
-    // 상품 카테고리
-    const [gift, setGift] = useState([{title:"상품권"}]);
-    
-
-    
-    let abc = 15
 
     const [friendList, setFriendList] = useState();
+    const searchInput = useRef(null)
 
     async function dataGet() {
         const res = await axios.get(`/api`)
@@ -31,28 +28,25 @@ const GiftTree = () => {
     }, [])
 
     console.log(friendList,'친구')
-
-    let userNumber = 1;
-    let userId = userNumber;
-    
-
+    // console.log(Math.floor(Math.random()*5)+1)
 
   return (
     <div style={{display:"flex",flexDirection:"column",position:"relative"}}>
         <div style={{width:"100%",maxWidth:"600px",minWidth:"300px", height:"90px",position:"fixed",top:0,left:"50%",transform:"translateX(-50%)",zIndex:10,background:'#ddd'}}>
           {/* ,position:"fixed",top:0,left:"50%",transform:"translateX(-50%)" */}
           <div>친구리스트</div>
-          <div style={{display:"flex",justifyContent:"space-around"}}>
+          <div style={{display:"flex",justifyContent:"space-around",alignItems:"center"}}>
           <ul style={{display:"flex", listStyle:"none"}}>
                 {friendList?.map((obj,idx)=>{
-                return <li key={idx} style={{width:60,height:30,border:"1px solid red", margin:"1%"}}><Link href={`/GiftTree/${obj.NickName}`}>{obj.NickName}</Link></li>
+                return <li key={idx} style={{width:60}}><Link href={`/GiftTree/${obj.NickName}`}><figure><img style={{width:40,borderRadius:"50%",border:`3px solid rgba(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},0.8)`}}  src={`/img/Profile${(Math.floor(Math.random()*5)+1)}.jpg`}/><figcaption><strong>{obj.NickName}</strong></figcaption></figure></Link></li>
                 })}
             </ul>
-
-            <form onSubmit={(e)=>{e.preventDefault()}}>
-                <input style={{width:50}} placeholder='친구 검색'/>
-                <button type="submit">돋보기</button>
+            <div style={{position:"relative"}}>
+            <form onSubmit={(e)=>{e.preventDefault()}} style={{position:"absolute",right:0,top:0,display:"flex",justifyContent:"center",alignItems:"center"}}>
+                <input ref={searchInput} style={{width:10}} placeholder='친구 검색'/>
+                <button onClick={()=>{console.log(searchInput.current.style={transform:"scale:1"})}} style={{width:30,height:30}} type="submit"><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
             </form>
+            </div>
           </div>
             
         </div>
