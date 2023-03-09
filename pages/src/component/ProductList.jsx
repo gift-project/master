06afdebe6a9f35be05554782b45e7acaa3af5c4f
Daehan930 <asdsad8835@gmail.com>
2023-03-09 18:21:23 +0900

    mod:ProdList

--- pages/src/component/ProductList.jsx
+++ pages/src/component/ProductList.jsx
@@ -4,17 +4,14 @@ import axios from 'axios';
 import { TeamC } from '../Context';
 
 const ProductList = () => {
-  
-  const [visible,setVisible] = useState(false);
 
+    const [visible,setVisible] = useState(false);
 
+    const maxLength = 10; // 문자열 길이 설정
 
     const arr = ["생일", "결혼", "환갑", "응원", "합격"]
 
     // 네이버 검색 api 호출
-
-
-
     const [thenApi, setThenApi] = useState();
 
     // 사러가기 클릭 -> 트리공간에 뿌려주기
@@ -76,7 +73,9 @@ const ProductList = () => {
             observer2.observe(bottom);
           }
         }, [thenApi]);
-  
+        
+
+        // 카테고리 아이템 처음부터 보여주는 코드
         useEffect( () => {
             if(visible)
             {GetApi()}},
@@ -122,20 +121,56 @@ const ProductList = () => {
 
       )
     })}
-    <div style={{border:"4px solid black",background:"#ddd",width:"100%",height:"70vh",position:"absolute",top:0,left:0,display:visible?"block":"none",overflow:"scroll"}}>
+
+      {/* 
+          카테고리 라디우스이미지 + 카테고리 이름 
+
+          카테고리 5개 이미지 구하샘 O
+
+          아이템들 물품제목, 가격  간결하게 보이게 ex 욕실 화장실 ... 
+                                             10,000                     
+          
+          타이틀 텍스트 한줄로 줄이기 
+          
+      */}
+    <div style={{border:"4px solid black", borderRadius:"5px",background:"#F2F2F2",width:"100%",height:"70vh",position:"absolute",top:0,left:0,display:visible?"block":"none",overflow:"auto"}}>
       <article style={{display:"flex",justifyContent:"space-around"}}>
-      <h3>쇼핑카트</h3>
+      {/* <h3>쇼핑카트</h3> */}
         <form onSubmit={ (e)=>{searchCg(e)} }>
-        <input />
-        <button type='submit'>검색</button>
+        <input 
+          type="text" 
+          style={{ 
+          borderRadius: "10px", // 라디우스 조정
+          padding: "10px", // 내부 여백 조정
+          fontSize: "16px", // 폰트 크기 조정
+          width: "400px", // 너비 조정
+          height: "50px", // 높이 조정
+          margin: "20px" 
+          }} 
+        />
+        <button type='submit'
+           style={{
+            backgroundColor: "#219bc3",
+            border: "none",
+            color: "white",
+            padding: "15px 20px;",
+            textAlign: "center",
+            textDecoration: "none",
+            display: "inline-block",
+            fontSize: "16px",
+            borderRadius: "10px",
+            cursor: "pointer"
+          }}
+        
+        >검색</button>
         </form>
       </article>
 
-        <ul style={{display:"flex", listStyle:"none", padding:"2%"}}>
+        <ul style={{display:"flex", listStyle:"none", padding:"2%", justifyContent:"space-around", alignItems:"center"}}>
 
         {arr.map((obj,idx)=>{
-            return <li onClick={ ()=>{GetApi(obj)} } style={{border:"1px solid coral", width:60,height:40,margin:"1.5%"}} 
-            key={idx}>{obj}</li>
+            return <li onClick={ ()=>{GetApi(obj)} } style={{border:"1px solid coral", width:60,height:60,margin:"1.5%", textAlign:"center",alignItems:"center", borderRadius:"50%" }} 
+            key={idx}> <figure> <img src={`/img/${obj}.jpg`} style={{width:45}}/> <figcaption>{obj}</figcaption> </figure> </li>
         })}
         </ul>
 
@@ -144,7 +179,8 @@ const ProductList = () => {
             return <figure key={idx} style={{width:"50%"}}>
             <img src={obj.image} style={{ width:"50%"}} />
                 <figcaption style={{width:"100%"}}>
-                <span>{obj.title.replaceAll("<b>","").replaceAll("</b>","")}</span>
+                <span>{obj.title.replaceAll("<b>","").replaceAll("</b>","").substr(0, maxLength) + (obj.title.length > maxLength ? "..." : "")}</span>
+                <span>{obj.lprice}</span>
                 <button onClick={()=>{window.open(`${obj.link}`, 'window_name', 'width=430, height=500, location=no, status=no,  scrollbars=yes')}}>
               자세히보기</button>                
             <button onClick={ ()=>{giveTo(obj)} }>선물하기</button>
