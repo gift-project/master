e2037bc67912dfbc5cdea8b1f72c5ce6ddbf1b1c
devyant <devvyves@gmail.com>
2023-03-14 16:22:30 +0900

    mypage_update

--- pages/MyPage/[id].jsx
+++ pages/MyPage/[id].jsx
@@ -21,33 +21,33 @@ const MyPage = () => {
     },[])
 
     return (
-    <div style={{paddingTop:"10%"}}>
-      <div style={{position:"fixed",top:"48%",left:"50%",transform:"translate(-50%,-50%)",zIndex:0,background:"rgba(255,255,255,0.6)",width:"550px",height:"90vh",borderRadius:"24px",boxShadow:"0 2px 2px"}}>
-        <div style={{width:250, marginBottom: "50px"}}>
-          <p style={{display: "block", width:150, height:150, margin: "0 auto"}}>
-            <img style={{width: "100%", height: "100%", borderRadius: "50%"}} src="/img/profile_picture04.jpg"/>
-          </p>
-          <p style={{marginTop: "20px", textAlign: "center"}}>유저이름</p>
+    <div style={{width: "100%", height: "100%", paddingTop:"10%", backgroundColor: "#f4f5f9"}}>
+      {/* <div style={{position:"fixed",top:"48%",left:"50%",transform:"translate(-50%,-50%)",zIndex:0,background:"rgba(255,255,255,0.6)",width:"100%", maxWidth: "600px" ,height:"90vh",borderRadius:"24px",boxShadow:"0 2px 2px"}}> */}
+
+        <div style={{width: "90%", margin: "0 auto 50px"}}>
+          <div>
+            <p style={{display: "block", width:200, height:200, margin: "0 auto", borderRadius: "50%",boxShadow: "0px 4px 10px 2px #ccc", border: "8px solid #fff"}}>
+              <img style={{width: "100%", height: "100%", borderRadius: "50%"}} src="/img/Profile4.jpg"/>
+            </p>
+            <p style={{marginTop: "20px", textAlign: "center", fontSize: "25px"}}>홍길동</p>
+          </div>
         </div>
         
-        <div style={{display: "flex", justifyContent: "center", marginBottom: "20px"}}>
-          <button onClick={()=>{setGiftVisible(false)}} style={{ backgroundColor: (giftVisible == false)? "#219bc3" : "#b2d3e1", display: "block", width: "30%", border: "none", margin: 0, padding: "14px 0", borderRadius: "15px 0 0 15px", borderRight: "1px solid #219bc3"}} >받은 선물</button>
-          <button onClick={()=>{setGiftVisible(true)}} style={{ backgroundColor: (giftVisible == true)? "#219bc3" : "#b2d3e1", display: "block", width: "30%", border: "none", margin: 0, padding: "14px 0", borderRadius: "0 15px 15px 0"}} >보낸 선물</button>
+        <div style={{display: "flex", justifyContent: "center", width: "90%",marginBottom: "20px"}}>
+          <button onClick={()=>{setGiftVisible(false)}} style={{ backgroundColor: (giftVisible == false)? "#219bc3" : "#b2d3e1", display: "block", width: "50%", border: "none", margin: 0, padding: "14px 0", borderRadius: "10px 0 0 10px", borderRight: "1px solid #219bc3"}} >받은 선물</button>
+          <button onClick={()=>{setGiftVisible(true)}} style={{ backgroundColor: (giftVisible == true)? "#219bc3" : "#b2d3e1", display: "block", width: "50%", border: "none", margin: 0, padding: "14px 0", borderRadius: "0 10px 10px 0"}} >보낸 선물</button>
         </div>
 
         <div style={{display:giftVisible?"none":"block",width:"100%"}}>
-          <p style={{padding: "20px 0", textAlign: "center"}}>내가 받은 선물</p>
-          <ul style={{display: "flex", flexWrap: "wrap", width: "90%", borderRadius: "20px", backgroundColor: "#fff",listStyle: "none",height:"30vh", overflow: "auto"}}>
-
+          <ul style={{display: "flex", flexDirection: "column", width: "90%", borderRadius: "10px", backgroundColor: "#fff",listStyle: "none",height: "40vh", overflow: "auto", padding: "12.5px 0", boxShadow: "0px 4px 10px 2px #ccc", border: "8px solid #fff"}}>
             {
               takeData && takeData.map((obj,idx )=>{
-                
-                return <li key={idx} style={{width: "50%", borderBottom : (idx == takeData.length - 2 || idx == takeData.length - 1) ? "none" : "1px solid #ccc"  }}> 
-                        <div style={{display: "flex", justifyContent: "flex-start", alignItems: "center" , padding: " 15px 10%"}}>
+                return <li key={idx} style={{width: "100%",margin: "0"}}> 
+                        <div style={{display: "flex", justifyContent: "flex-start", alignItems: "center",width: "100%" ,padding: " 12.5px 10%"}}>
                           <img src={obj.image} alt="gift" style={{display: "block", width: "70px", height: "70px", margin: 0, borderRadius: "50%", backgroundColor: "orange"}}/>
-                          <div style={{margin: 0, paddingLeft: "20px"}}>
-                            <p style={{fontSize: "14px"}}>{obj.title}</p>
-                            <p style={{margin: "3px 0", fontSize: "12px"}}>{obj.price}</p>
+                          <div style={{width: "calc(100% - 70px)", margin: 0, paddingLeft: "20px"}}>
+                            <p style={{width: "100%", fontSize: "14px",overflow: "hidden" ,textOverflow: "ellipsis", whiteSpace: "nowrap"}}>{obj.title}</p>
+                            <p style={{margin: "7px 0", fontSize: "12px"}}>{obj.price}</p>
                             <p style={{fontSize: "12px"}}>{obj.GiverName}에게 받음</p>
                           </div>
                         </div>
@@ -60,18 +60,16 @@ const MyPage = () => {
         {/* 받은 선물 */}
 
         <div style={{display:giftVisible?"block":"none", width:"100%"}}>
-          <p style={{padding: "20px 0", textAlign: "center"}}>내가 보낸 선물</p>
-          <ul style={{display: "flex", flexWrap: "wrap", width: "90%", borderRadius: "20px", backgroundColor: "#fff",listStyle: "none",height:"30vh", overflow: "auto"}}>
-
+          <ul style={{display: "flex", flexDirection: "column", width: "90%", borderRadius: "10px", backgroundColor: "#fff",listStyle: "none",height: "40vh", overflow: "auto", padding: "12.5px 0", boxShadow: "0px 4px 10px 2px #ccc", border: "8px solid #fff"}}>
             {
               takeData && takeData.map((obj,idx )=>{
-                return <li key={idx} style={{width: "50%", borderBottom : (idx == takeData.length - 2 || idx == takeData.length - 1) ? "none" : "1px solid #ccc"  }}> 
-                      <div style={{display: "flex", justifyContent: "flex-start", alignItems: "center" , padding: " 15px 10%"}}>
+                return <li key={idx} style={{width: "100%",margin: "0"}}> 
+                      <div style={{display: "flex", justifyContent: "flex-start", alignItems: "center",width: "100%" ,padding: " 12.5px 10%"}}>
                         <img src={obj.image} alt="gift" style={{display: "block", width: "70px", height: "70px", margin: 0, borderRadius: "50%", backgroundColor: "orange"}}/>
-                        <div style={{margin: 0, paddingLeft: "20px"}}>
-                        <p style={{fontSize: "14px"}}>{obj.title}</p>
-                            <p style={{margin: "3px 0", fontSize: "12px"}}>{obj.price}</p>
-                            <p style={{fontSize: "12px"}}>{obj.UserName}에게 보냄</p>
+                        <div style={{width: "calc(100% - 70px)", margin: 0, paddingLeft: "20px"}}>
+                          <p style={{width: "100%", fontSize: "14px",overflow: "hidden" ,textOverflow: "ellipsis", whiteSpace: "nowrap"}}>{obj.title}</p>
+                          <p style={{margin: "7px 0", fontSize: "12px"}}>{obj.price}</p>
+                          <p style={{fontSize: "12px"}}>{obj.UserName}에게 보냄</p>
                         </div>
                       </div>
                     </li>
@@ -82,7 +80,7 @@ const MyPage = () => {
         </div>
 
         {/* 보낸 선물 */}
-        </div>
+        {/* </div> */}
         <NavBar />
     </div>
   )
---
+++ pages/Present.js
@@ -0,0 +1,60 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+
+
+const Present = () => {
+
+  const [friendBday, setFriendBday] = useState([]);
+
+  async function friendsBirth() {
+    const res = await axios.get(`/api`)
+    const data = res.data
+    setFriendBday(data)
+  }
+
+  useEffect(() => {
+    friendsBirth()
+  }, [])
+
+
+
+
+
+  // const ddd = (friendBday[0].Birth);
+  if(friendBday.length > 0){
+    // console.log(friendBday[0].Birth, "생일")
+    const datee = new Date(friendBday[0].Birth);
+    const datee2 = new Date();
+    if(datee2 > datee){
+      console.log('가깝다')
+    }else{
+      console.log('멀다')
+    }
+  } 
+
+  console.log(friendBday)
+
+  console.log(  24*60*60*1000 )
+
+
+  // if(ddd > datee){
+  //   console.log('생일이 가까워져')
+  // }else{
+  //   console.log('생일이 멀어져')
+  // }
+
+  // console.log(datee)
+
+
+  return (
+    <div>
+      {/* {
+        friendBday.map((obj, idx) => {
+          return <div key={}></div>
+        })
+      } */}
+    </div>
+  );
+};
+
+export default Present;
